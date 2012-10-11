c2467011f8a53d038572e2e87b5d4ff5b9307dc4
Gabriel Marte <gabrielmarte@Gabriels-MacBook-Pro.local>
2012-10-11 18:00:30 -0400

    Added project 3 folder

---
+++ sdi-project3/json.js
@@ -0,0 +1,23 @@
+//JS Data for project 3
+//Author: Gabriel Marte
+
+
+var json = {
+	"players":[
+		{	
+			"playerName": "Kring",
+			"class": "Warrior",
+			"rank": "General"
+		},
+		{
+			"playerName": "Morduke",
+			"class": "Mage",
+			"rank": "Wizard"
+		},
+		{
+			"playerName": "Rengu",
+			"class": "Assassin",
+			"rank": "Elite"
+		}
+	]
+};
