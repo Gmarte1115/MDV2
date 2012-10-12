30f1c1b973bc492331baa7dd1225b8eab7e71c9a
Gabriel Marte <gabrielmarte@new-host.home>
2012-10-12 14:46:49 -0400

    Output conditional statement that displays fight outcomes based on hp calculation of 2 characters and their given data

--- sdi-project3/marte_gabriel_project3.js
+++ sdi-project3/marte_gabriel_project3.js
@@ -1,15 +1,22 @@
 //Author: Gabriel Marte
 //SDI project 3
 //10/11/2012
-var newGame
+
+var teamNames = ["Warriors", "Sorcerers", "Assassins"];
+var evilBattleCry = "We will destroy you!! BWAHAHAHAHAHA";
+var goodBattleCry = "Thou shall not pass this way...";
+var evilVictoryMsg = "BWAHAHAHAHA, YOU DID NOT STAND A CHANCE!";
+var goodVictoryMsg = "THY HAS TOLDETH YOU SO!!!";
+var beginBattleMsg = "FIGHT!";
+
+
 var getCharList = function(json){
+	var characters = json.characters
 	for (var i = 0; i<json.characters.length; i++){		//FOR LOOP
 		var characters = json.characters[i]					//LOCAL VARIABLES
 		console.log("Player: " + characters.charName + ", Rank: " + characters.charRank + ", Class: " + characters.charClass )
-
-	}
+	}return characters
 };
-
 
 
 var getUpGrade = function(json){
@@ -17,6 +24,7 @@ var getUpGrade = function(json){
 		var equipUpGrade = json2.upGrades[i]			//LOCAL VARIABLES
 		console.log("If you choose the " + equipUpGrade.weapon + ", you gain " + equipUpGrade.powerUp)
 	}
+	return equipUpGrade
 };
 
 //Constructor
@@ -40,6 +48,9 @@ var newChar = function (name){
 	}
 };
 
+getCharList(json)
+getUpGrade(json)
+
 //Kring info
 var kring = newChar("Kring");
 console.log (kring);
@@ -48,7 +59,7 @@ kring.addData("MP: 10");
 kring.addData("20,000 soldiers");
 kring.addData("2 revive potions");
 kring.combatReady = true
-kring.totalHp = 20
+kring.totalHp = 30
 kring.getCharData();
 
 var morduke = newChar("Morduke");
@@ -70,3 +81,29 @@ rengu.addData("2 revive potions");
 rengu.combatReady = true
 rengu.totalHp = 25
 rengu.getCharData();
+
+console.log ("'Kring vs. Morduke'")
+if (kring.totalHp > morduke.totalHp){
+	console.log ("Kring: " + goodBattleCry)
+	console.log ("Morduke: " + evilBattleCry)
+	console.log (beginBattleMsg)
+	console.log ("Kring: " + goodVictoryMsg)
+} else {
+	console.log ("Kring: " + goodBattleCry)
+	console.log ("Morduke: " + evilBattleCry)
+	console.log (beginBattleMsg)
+	console.log ("Morduke: " + evilVictoryMsg)
+};
+		
+
+
+
+
+
+
+
+
+
+
+
+
