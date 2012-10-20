c754cdcac92ae1eddeda3b315e2d8be20a4a6bf7
Gabriel Marte <gabrielmarte@new-host-3.home>
2012-10-19 22:28:14 -0400

    Added flowchart images, noted .js file with corresponding flowcharts

--- sdi-project4/marte_gabriel_project4.js
+++ sdi-project4/marte_gabriel_project4.js
@@ -6,7 +6,7 @@
 //FUNCTIONS LIBRARY
 
 //Does a string follow a 123-456-7890 pattern like a phone number?v
-
+//STRING FUNCTION FLOWCHART
 var valPhoneNumFormat = function(checkNum){
 	var phoneNum = checkNum;
 	var phonePattern = /^[0-9-]+[0-9-]+[0-9-]/;
@@ -14,12 +14,13 @@ var valPhoneNumFormat = function(checkNum){
 		console.log ("Phone number format is correct");
 	} else {console.log("Phone number format is incorrect");
 	   };
+	return phoneNum;
 };
 
 valPhoneNumFormat("111-111-1111");
 
 //Does a string follow an aaa@bbb.ccc pattern like an email address?
-
+//STRING FUNCTION FLOWCHART
 
 var valEmailFormat = function(checkEmail){
 	var emailSample = checkEmail;
@@ -28,12 +29,13 @@ var valEmailFormat = function(checkEmail){
 		console.log ("Email format is correct");
 	} else {console.log("Email format is incorrect");
 	   };
+	return emailSample;
 };
 
 valEmailFormat("aaa@bbb.ccc")
 
 //Is the string a URL? (Does it start with http: or https:?)
-
+//STRING FUNCTION FLOWCHART
 var valUrl = function(checkUrl){
 	var urlSample = checkUrl;
 	var urlPattern = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
@@ -41,12 +43,13 @@ var valUrl = function(checkUrl){
 		console.log ("URL format is correct");
 	} else {console.log("URL format is incorrect");
 	   };
+	return urlSample;
 };
 
 valUrl("https://www.test.com")
 
 //Title-case a string (split into words, then uppercase the first letter of each word)
-
+//STRING FUCTION WITH LOOP FLOWCHART
 function upCase(string) {
     var testString = string.split(/\s|_/);
     for(var i=0,l=testString.length; i<l; i++) {
@@ -59,7 +62,7 @@ function upCase(string) {
 console.log(upCase("testing this"));
 
 //Find the smallest value in an array that is greater than a given number
-
+//ARRAY FUNCTION FLOWCHART
 var minValue = function (array) {
 	var	min = array[0];
     for (i=0; i<array.length; i++) {
@@ -70,10 +73,10 @@ var minValue = function (array) {
     return min;
 };
 
-console.log (minValue([2, 4, 6, 8, 10]));
+console.log ("The smallest value is " + (minValue([2, 4, 6, 8, 10])));
 
 //Find the total value of just the numbers in an array, even if some of the items are not numbers.
-
+//ARRAY FUNCTION FLOWCHART
 var totalVal = function (array) {
 	var sum = 0;
 	for (var i=0; i<array.length; i++) {
@@ -81,9 +84,10 @@ var totalVal = function (array) {
 		console.log(sum)
 		};
 	};
+	return sum;
 };
 
-totalVal([2, 4, 6, 8, 10, "a", "b", "c", "d", "e"])
+totalVal([1, 2, 3, 4, 5, "a", "b", "c", "d", "e"])
 
 
 
